b1e8ca8e1572c1afc5ab99e5a6c9f4292183d805
り よ <s175anb185@gmail.com>
2026-01-02 10:15:32 +0900

    fix: add meal_date to apiAddKondate type

---
+++ simple-kondate/src/app/main/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>家族アプリ</h1>
+      <ul>
+        <li>
+          <a href="/kondates">🍱 献立アプリ</a>
+        </li>
+      </ul>
+    </main>
+  );
+}
