dc01a60775c00603e57bf83b9992a4dd1a5ecd2e
り よ <s175anb185@gmail.com>
2026-01-03 14:50:01 +0900

    fix: add meal date to apiAddKondate type

--- simple-kondate/src/types/recipe.ts
+++ simple-kondate/src/types/recipe.ts
@@ -1,17 +1,17 @@
-export type Recipe = {
-    id: string;
-    title: string;
-    description?: string | null;
-    timeMinutes?: number | null;
-    tags?: string[] | null;
-    mainCategory?: string;
-};
-
 export type ApiRecipe = {
     id: string;
     title: string;
     description?: string | null;
     time_minutes?: number | null;
     tags?: string[] | null;
-    main_Category?: string;
+    main_category?: string;
+};
+
+export type Recipe = {
+  id: string;
+  title: string;
+  description?: string | null;
+  timeMinutes?: number | null;
+  tags?: string[] | null;
+  mainCategory: string; // ★こっちは必須推奨
 };
